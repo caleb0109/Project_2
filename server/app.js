2634fe006a8ba8d5b99c7990dee1d0bd8f1c957a
caleb0109 <70354203+caleb0109@users.noreply.github.com>
2024-05-02 22:52:44 -0400

    got ride of favicon

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const compression = require('compression');
-const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
@@ -35,7 +34,6 @@ const app = express();
 redisClient.connect().then(() => {
   app.use(helmet());
   app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
-  app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
   app.use(compression());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
